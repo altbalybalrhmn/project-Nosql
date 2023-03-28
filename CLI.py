d3f6a41d55c90691a8bcd84ab7fe42c7b6d3d026
Abdulrhman Altabali <70818818+altbalybalrhmn@users.noreply.github.com>
2023-03-29 01:18:23 +0200

    here we go
    ready

--- CLI.py
+++ CLI.py
@@ -4,6 +4,7 @@ import typer
 from pymongo import MongoClient, DESCENDING
 from pprint import pprint
 
+
 # Provide the mongodb atlas url to connect python to mongodb using pymongo
 CONNECTION_STRING = "mongodb+srv://altbalybalrhmn:CpnooyI6kvbUM7uz@freestudentcluster.kzvfjxb.mongodb.net/?retryWrites=true&w=majority"
 
@@ -25,13 +26,13 @@ app = typer.Typer()
 #####################################################################
 
 # sign up
-@app.command()
+@app.command('signup')
 def signup(user_name: str):
 
     try:
         user_collection.insert_one(
             {
-                'user_name': user_name,
+                '_id': user_name,
                 'history': {
 
                     'books_read': [],
@@ -52,11 +53,11 @@ def signup(user_name: str):
 ########################################################################
 
 # sign in
-@app.command()
+@app.command('signin')
 def signin(user_name: str):
 
     try:
-        results = user_collection.find_one({'user_name': user_name})
+        results = user_collection.find_one({'_id': user_name})
         for i in results:
             print(f'your posts are {i}')
         print(f'welcome {user_name}. You are logged in')
@@ -67,7 +68,7 @@ def signin(user_name: str):
 ########################################################################
 
 # add a book
-@app.command()
+@app.command('add_book')
 def add_book(book_id: int, book_name: str, genre: str, author: str):
 
     book_collection.insert_one({
@@ -86,14 +87,14 @@ def add_book(book_id: int, book_name: str, genre: str, author: str):
 ########################################################################
 
 # borrow a book
-@app.command()
+@app.command('borrow_book')
 def borrow_book(user_name: str, book_id: int):
 
     book_collection.update_one(
         {'status': ''}, {'$set': {'status': 'borrowed'}})
     book_collection.update_one(
         {'borrower': ''}, {'$set': {'borrower': user_name}})
-    user_collection.update_one({'user_name': user_name}, {
+    user_collection.update_one({'_id': user_name}, {
                                '$push': {'history.borrowing_now': book_id}})
     print(f' book with id {book_id} was borrowed by {user_name}')
 
@@ -101,16 +102,16 @@ def borrow_book(user_name: str, book_id: int):
 ############################################################
 
 # return a book
-@app.command()
+@app.command('return_book')
 def return_book(user_name: str, book_id: int):
 
     book_collection.update_one({'status': 'borrowed'}, {
                                '$set': {'status': ''}})
     book_collection.update_one({'borrower': user_name}, {
                                '$set': {'borrower': ''}})
-    user_collection.update_one({'user_name': user_name}, {
+    user_collection.update_one({'_id': user_name}, {
                                '$pull': {'history.borrowing_now': book_id}})
-    user_collection.update_one({'user_name': user_name}, {
+    user_collection.update_one({'_id': user_name}, {
                                '$push': {'history.books_read': book_id}})
     print(f' book with id {book_id} was returned by {user_name}')
 
@@ -118,10 +119,10 @@ def return_book(user_name: str, book_id: int):
 ###########################################################
 
 # add favourit book
-@app.command()
+@app.command('add_favorite_book')
 def add_favorite_book(user_name, book_id):
     user_collection.update_one(
-        {'user_name': user_name}, {'$push': {'history.fav_books': book_id}})
+        {'_id': user_name}, {'$push': {'history.fav_books': book_id}})
     print(
         f' book with id {book_id} was categorized as favourite by user {user_name}')
 
@@ -129,7 +130,7 @@ def add_favorite_book(user_name, book_id):
 ############################################################
 
 # recently added n books
-@app.command()
+@app.command('recently_added')
 def recently_added(n: int):
     select = {'$project': {'book_id': 1, 'book_name': 1}}
     all = {'$match': {}}  # filter
@@ -144,8 +145,8 @@ def recently_added(n: int):
 ###############################################################
 
 # search by book name
-@app.command()
-def search_name_book(book_name):
+@app.command('search_name_book')
+def search_name_book(book_name: str):
     filter = {
         '$match': {'book_name': book_name}
     }
@@ -155,11 +156,13 @@ def search_name_book(book_name):
     for result in results:
         print(result)
 
+###################################################################
+
 # search by author name
 
 
-@app.command()
-def search_author_book(author):
+@app.command('search_author_book')
+def search_author_book(author: str):
 
     filter = {
         '$match': {'author': author}
@@ -174,12 +177,12 @@ def search_author_book(author):
 ###############################################################
 
 # the most three readers
-@app.command()
-def book_read_count(n):
+@app.command('book_read_count')
+def book_read_count(n: int):
     stage_group_read_count_by_book_name = {
         "$group": {
 
-            "_id": "$user_name",
+            "_id": "$_id",
             "no_reads": {"$sum": {"$size": "$history.books_read"}
                          }}
     }
@@ -201,8 +204,8 @@ def book_read_count(n):
 # most available genres
 
 
-@app.command()
-def most_available_genres(n):
+@app.command('most_available_genres')
+def most_available_genres(n: int):
     group_stage = {
         '$group': {'_id': '$genre',
                    "no_books": {"$sum": 1}
@@ -227,7 +230,7 @@ def most_available_genres(n):
 # top_publishing_authors
 
 
-@app.command()
+@app.command('top_publisher_authors')
 def top_publisher_authors(n):
 
     group_stage = {
@@ -253,10 +256,10 @@ def top_publisher_authors(n):
 # user_statistics
 
 
-@app.command()
-def statistics(user_name):
+@app.command('statistics')
+def statistics():
     group_stage = {
-        '$group': {'_id': '$user_name',
+        '$group': {'_id': '$_id',
                    'reads': {'$sum': {'$size': '$history.books_read'}},
                    'favourte': {'$sum': {'$size': '$history.fav_books'}},
                    'borrowing_now': {'$sum': {'$size': '$history.borrowing_now'}}
